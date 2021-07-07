a3493f24cf4c3e46a26401539ff203fbeda1ac97
omon03 <omon03@gmail.com>
2021-07-08 02:03:00 +0500

    Initial commit

---
+++ src/main/java/com/pm/binance_rest/controllers/Binance.java
@@ -0,0 +1,8 @@
+package com.pm.binance_rest.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Binance {
+
+}
---
+++ src/test/java/com/pm/binance_rest/BinanceRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pm.binance_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinanceRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
